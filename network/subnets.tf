c558e08b0ba1aafe5512d81341a6511b03859c1b
Abdelrhman Sayed <58826560+abdelrhman95@users.noreply.github.com>
2023-10-29 05:40:57 +0200

    Update subnets.tf

--- network/subnets.tf
+++ network/subnets.tf
@@ -2,12 +2,12 @@
 resource "google_compute_subnetwork" "managment_subnet" {
 name          = "managment-subnet"
 ip_cidr_range = var.managment_subnet_cider
-region        = var.vpc-region
+region        = var.managment-subnet-region
 
 network       = google_compute_network.my-vpc.self_link  #Using self_link avoids issues with resource name changes
 description = "Managment subnet"
 
-private_ip_google_access = true
+#private_ip_google_access = true
 
 }   
 
@@ -15,11 +15,11 @@ private_ip_google_access = true
 resource "google_compute_subnetwork" "workload_subnet" {
 name          = "workload-subnet"
 ip_cidr_range = var.workload_subnet_cider
-region        = var.vpc-region
+region        = var.workload-subnet-region
 
 network       = google_compute_network.my-vpc.self_link  #Using self_link avoids issues with resource name changes
 description = "workload subnet for GKE"
 
-private_ip_google_access = true
+#private_ip_google_access = true
 
 }
