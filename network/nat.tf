70800cbbf51b9ba6442c2e5d4b63a67a7639c902
Abdelrhman Sayed <58826560+abdelrhman95@users.noreply.github.com>
2023-10-29 05:39:28 +0200

    Update nat.tf

--- network/nat.tf
+++ network/nat.tf
@@ -4,8 +4,12 @@ resource "google_compute_router_nat" "nat" {
     router     = google_compute_router.router.name
     region     = google_compute_router.router.region
     nat_ip_allocate_option   = var.nat_ip_allocate_option
-    source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES" # used by vm instances
+    source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"  # used by vm instances
 
+subnetwork {
+      name = google_compute_subnetwork.managment_subnet.id
+      source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+    }
 
     log_config {
     enable = true
