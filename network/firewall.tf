463f80c34ed6b8f1968aa15c58db4451380bff14
Abdelrhman Sayed <58826560+abdelrhman95@users.noreply.github.com>
2023-10-29 05:37:31 +0200

    Update firewall.tf

--- network/firewall.tf
+++ network/firewall.tf
@@ -2,7 +2,8 @@
 resource "google_compute_firewall" "allow-iap-ssh" {
   name    = "allow-iap-ssh"
   network = google_compute_network.my-vpc.name
-  
+  direction = "INGRESS"
+
 
   allow {
     protocol = "tcp"
@@ -11,7 +12,7 @@ resource "google_compute_firewall" "allow-iap-ssh" {
 
   source_ranges = [
     "35.235.240.0/20", 
-    google_compute_subnetwork.managment_subnet.ip_cidr_range
+    #google_compute_subnetwork.managment_subnet.ip_cidr_range
   ] #  the current IAP TCP forwarding IP range
   
   # target_tags = [  ] # maybe i need it later usage to specify which machine should this firewall use & i have to use the same tag i created in my vm tage section
