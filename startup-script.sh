dddd20854437279570f7499444c838dc10a05d08
Abdelrhman Sayed <58826560+abdelrhman95@users.noreply.github.com>
2023-11-02 19:34:13 +0200

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -2,27 +2,23 @@
 
 
 #install docker 
-sudo apt update
-sudo apt install apt-transport-https ca-certificates gnupg2 software-properties-common
+# sudo apt update
+# sudo apt install apt-transport-https ca-certificates gnupg2 software-properties-common
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+# curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# sudo apt update
+# sudo apt install docker-ce docker-ce-cli containerd.io
 
-sudo systemctl start docker
-sudo systemctl enable docker
+# sudo systemctl start docker
+# sudo systemctl enable docker
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
 
-
-
-
-# Kubectl Installation
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
 
 
 # gcloud CLI Installation
@@ -33,7 +29,20 @@ sudo apt-get update && sudo apt-get install google-cloud-cli
 sudo apt-get install google-cloud-cli-gke-gcloud-auth-plugin
 gcloud init
 
+# Kubectl Installation
+# sudo apt-get update
+# sudo apt-get install -y apt-transport-https ca-certificates curl
+# sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+# echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+# sudo apt-get update
+# sudo apt-get install -y kubectl
+sudo apt-get install kubectl
+sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
+
 
 
 #credentials of gke
 gcloud container clusters get-credentials my-gke-cluster --zone asia-east1 --project "molten-snowfall-401102"
+
+#auth for artifact repo
+gcloud auth print-access-token | sudo docker login -u oauth2accesstoken --password-stdin https://asia-east1-docker.pkg.dev
